ad7ed2abbae799bd61b9efb8ef5ad583fa451c76
bmhungqb <21522110@gm.uit.edu.vn>
2023-12-26 08:02:18 +0700

    ~~

--- DNN/deploy_gradio.py
+++ DNN/deploy_gradio.py
@@ -2,19 +2,25 @@ import gradio as gr
 from inference_gradio import model_SemEval, model_davidson, process_input
 import numpy as np
 import os
-def process_output(predictions):
+def process_output_sem(predictions):
     if np.argmax(np.array(predictions)):
         return "Hate speech"
     else:
         return "Non-hate speech"
+def process_output_dad(predictions):
+    if np.argmax(np.array(predictions)):
+        return "Non-hate speech"
+    else:
+        return "Hate speech"
 def process_text(input_text, dataset):
     if dataset == "SemEval":
         input = process_input(input_text, vocab_path= os.path.join(path_model_SemEval, "vocab.pkl"))
         predictions = SemEval(input)
+        return process_output_sem(predictions)
     else:
         input = process_input(input_text, vocab_path= os.path.join(path_model_davidson, "vocab.pkl"))
         predictions = davidson(input)
-    return process_output(predictions)
+        return process_output_dad(predictions)
 
 demo = gr.Interface(
     fn=process_text,
